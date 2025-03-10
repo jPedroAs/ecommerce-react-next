acbbaf1fec462f93ed10b13ea14fef017addb163
jPedroAs <joaopedroa2120@outlook.com>
2025-03-10 16:17:43 -0300

    nova url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:"https://dgdtpfbwoa.execute-api.sa-east-1.amazonaws.com",
+    baseURL:"https://eiif5bjnih.execute-api.us-east-1.amazonaws.com",
     headers: {
-        'Content-Type': 'application/json', // Garante que o tipo de conteúdo seja JSON
+        'Content-Type': 'application/json',
     }
 })
 
