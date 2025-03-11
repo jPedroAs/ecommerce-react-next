5d5d499c4f067a368121ab19b04e5aeeb0c2da61
Pablo Lima <pablolimacoder@gmail.com>
2025-03-11 19:04:07 -0300

    Update MainBar.tsx

--- src/components/MainBar/MainBar.tsx
+++ src/components/MainBar/MainBar.tsx
@@ -4,6 +4,7 @@ import Link from "next/link"
 import search from "../../assets/Search.svg"
 import cart from "../../assets/Shopping cart.svg"
 import user from "../../assets/User.svg"
+import ModalCart from "../components/ModalCart/Cart";
 import { useState } from "react"; // Importe useState
 
 const MainBar = () => {
@@ -15,6 +16,15 @@ const MainBar = () => {
         setNavHeight(navHeight === "80px" ? "120px" : "80px"); // Altera a altura
         setIsVisible(!isVisible); // Inverte a visibilidade
     };
+    
+    const [modalOpen, setModalOpen] = useState(false);
+    function handlerModal() {
+        setModalOpen(!modalOpen)
+    }
+
+    async function handleCart() {
+        setModalOpen(true);
+    }
 
     return (
         <nav className={styles.nav} style={{ height: navHeight }}>
@@ -34,11 +44,11 @@ const MainBar = () => {
 
                 <div className={styles.icons}>
                     <button className={styles.choissesicons}  onClick={toggleNavHeight}><img src={search.src} id={styles.search}/></button>
-                    <button className={styles.choissesicons}><img src={cart.src}/></button>
+                    <button onClick={handleCart} className={styles.choissesicons}><img src={cart.src}/></button>
                     <Link href="/User" className={styles.choissesicons}><img src={user.src}/></Link>
                 </div>
             </div>
-            
+            <ModalCart isOpen={modalOpen} onClosed={handlerModal} />
             {isVisible && <div className={styles.searchbar}><input type="text" placeholder="Pesquise Aqui"/></div>}
         </nav>
     );
