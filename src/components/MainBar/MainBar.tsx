c8c161f6a497a47b8907427765d54ef37efdabc1
Pablo Lima <pablolimacoder@gmail.com>
2025-03-11 19:20:36 -0300

    Update MainBar.tsx

--- src/components/MainBar/MainBar.tsx
+++ src/components/MainBar/MainBar.tsx
@@ -17,42 +17,31 @@ const MainBar = () => {
         setIsVisible(!isVisible); // Inverte a visibilidade
     };
     
-    const [modalOpen, setModalOpen] = useState(false);
-    function handlerModal() {
-        setModalOpen(!modalOpen)
-    }
-
-    async function handleCart() {
-        setModalOpen(true);
-    }
 
     return (
-        <div>
-            <nav className={styles.nav} style={{ height: navHeight }}>
-                <div className={styles.container}>
+        <nav className={styles.nav} style={{ height: navHeight }}>
+            <div className={styles.container}>
     
-                    <div className={styles.logo}>
-                        <img src={logo.src} />
-                        <h1>UniStore</h1>
-                    </div>
-    
-                    <div className={styles.choisses}>
-                        <Link href="/Home" className={styles.link}>Home</Link>
-                        <Link href="/Produtos" className={styles.link}>Products</Link>
-                        <Link href="/Categories" className={styles.link}>Categories</Link>
-                        <Link href="/Review" className={styles.link}>Review</Link>
-                    </div>
-    
-                    <div className={styles.icons}>
-                        <button className={styles.choissesicons}  onClick={toggleNavHeight}><img src={search.src} id={styles.search}/></button>
-                        <button onClick={handleCart} className={styles.choissesicons}><img src={cart.src}/></button>
-                        <Link href="/User" className={styles.choissesicons}><img src={user.src}/></Link>
-                    </div>
+                <div className={styles.logo}>
+                    <img src={logo.src} />
+                    <h1>UniStore</h1>
                 </div>
-                {isVisible && <div className={styles.searchbar}><input type="text" placeholder="Pesquise Aqui"/></div>}
-            </nav>
-            <ModalCart isOpen={modalOpen} onClosed={handlerModal} />
-        </div>
+    
+                <div className={styles.choisses}>
+                    <Link href="/Home" className={styles.link}>Home</Link>
+                    <Link href="/Produtos" className={styles.link}>Products</Link>
+                    <Link href="/Categories" className={styles.link}>Categories</Link>
+                        Link href="/Review" className={styles.link}>Review</Link>
+                </div>
+    
+                <div className={styles.icons}>
+                    <button className={styles.choissesicons}  onClick={toggleNavHeight}><img src={search.src} id={styles.search}/></button>
+                    <button className={styles.choissesicons}><img src={cart.src}/></button>
+                    <Link href="/User" className={styles.choissesicons}><img src={user.src}/></Link>
+                </div>
+            </div>
+            {isVisible && <div className={styles.searchbar}><input type="text" placeholder="Pesquise Aqui"/></div>}
+        </nav>
     );
 };
 
