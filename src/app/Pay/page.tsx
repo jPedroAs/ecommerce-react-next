fc8ddd6f6cb49173a52162e14f1344b1ae466512
jPedroAs <joaopedroa2120@outlook.com>
2025-03-18 16:41:11 -0300

    add pagamento

--- src/app/Pay/page.tsx
+++ src/app/Pay/page.tsx
@@ -1,120 +1,207 @@
-'use client';
+"use client";
 
-import { useState, useEffect } from 'react';
+import api from "../../services/api"
+import { useEffect, useRef, useState } from "react";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements, useStripe, useElements, CardNumberElement, CardExpiryElement, CardCvcElement, CardElement } from "@stripe/react-stripe-js";
+import "./index.css";
+import MainBar from "@/components/MainBar/MainBar";
+import { Pedidos } from "@/Types/PedidoInterface";
+import { jwtDecode } from "jwt-decode";
 import Swal from "sweetalert2";
+import Link from "next/link";
 
-export default function PaymentRegistration() {
-    const [paymentMethod, setPaymentMethod] = useState<string>("");
+const stripePromise = loadStripe("pk_test_51Qz3IoFY2T2EW3rQl4qfXPbjp27xZK9vlCMl9pIBvdJd6UfPhpH18FYMIQxWvKWjy0yfM2ea0wLeqHZufFASXMyo0060uTKfRP");
 
-    // Verificar se o método de pagamento está armazenado no localStorage
-    useEffect(() => {
-        const savedPaymentMethod = localStorage.getItem("paymentMethod");
-        if (savedPaymentMethod) {
-            setPaymentMethod(savedPaymentMethod);
+const CARD_OPTIONS = {
+  style: {
+    base: {
+      display: "block",
+      fontSize: "16px",
+      color: "#17171c",
+      "::placeholder": { color: "#3c4249" },
+      letterSpacing: "0.025em",
+      padding: "10px",
+      borderRadius: "8px", // Adicionando borda arredondada
+      border: "1px solid #ccc", // Borda padrão
+    },
+    invalid: {
+      color: "#fa755a",
+    },
+  },
+  disableLink: true,
+};
+
+const CheckoutForm = () => {
+  const inputName = useRef<HTMLInputElement>(null);
+  const inputCPF = useRef<HTMLInputElement>(null);
+
+
+  const stripe = useStripe();
+  const [pedido, setPedido] = useState<Pedidos[]>([]);
+  const elements = useElements();
+  const [paymentMethod, setPaymentMethod] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [paymentType, setPaymentType] = useState<string | null>(null);
+  const [cardNumberKey, setCardNumberKey] = useState(0);
+
+  const handlePaymentTypeSelect = (type: string) => {
+    setPaymentType(type);
+  };
+
+  useEffect(() => {
+    setCardNumberKey(prevKey => prevKey + 1);
+  }, [paymentType]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        console.error("Token não encontrado.");
+        return;
+      }
+      let decodedToken: any;
+      try {
+        decodedToken = jwtDecode(token);
+      } catch (error) {
+        console.error("Erro ao decodificar token:", error);
+        return;
+      }
+      const responsePedido = await api.get(`/Pedido/${decodedToken.ID}`).then((responsePedido) => {
+        if (Array.isArray(responsePedido.data.pedido)) {
+          const data = responsePedido.data.pedido;
+          setPedido(data);
+        } else {
+          setPedido([]);
         }
-    }, []);
+      }).catch(() => {
+        setPedido([]);
+      })
+    }
+    fetchProducts();
+  }, []);
 
-    const handlePayment = () => {
-        if (!paymentMethod) {
-            Swal.fire({
-                text: "Por favor, selecione um método de pagamento!",
-                icon: "warning",
-            });
-            return;
-        }
+  const totalAmount = Array.isArray(pedido) ? pedido.reduce((sum, pedido) => sum + (pedido.produtos.preco * (pedido.pedidos.qnt_prod_unidade || 1)), 0).toFixed(2) : '0.00';
 
-        // Armazenar o método de pagamento no localStorage
-        localStorage.setItem("paymentMethod", paymentMethod);
+  
+  async function PostPagamento (e: React.FormEvent) {
+    e.preventDefault();
+    setError(null);
 
+    if (!stripe || !elements) {
+      setError("Stripe não foi carregado.");
+      return;
+    }
+
+    // const cardNumberElement = elements.getElement(CardNumberElement);
+    // const cardExpiryElement = elements.getElement(CardExpiryElement);
+    // const cardCvcElement = elements.getElement(CardCvcElement);
+    const cardElement = elements.getElement(CardElement);
+
+    if (!cardElement) {
+      setError("Erro ao obter o campo do cartão.");
+      return;
+    }
+
+    const { paymentMethod, error } = await stripe.createPaymentMethod({
+      type: "card",
+      card: cardElement,
+    });
+
+    if (error) {
+      console.log(error)
+    }
+    const nome = inputName.current?.value || "";  
+    const cpf = inputCPF.current?.value || "";  
+
+    const body = {
+      name: nome,
+      cpf: cpf,
+      Card_token: paymentMethod?.id,
+      amount:  Math.round(parseFloat(totalAmount) * 100),
+      type_payment: paymentType
+    }
+    console.log(body)
+
+    const response = await api.post("/Pay", body)
+      .then((response) => {
         Swal.fire({
-            text: `Pagamento realizado com sucesso via ${paymentMethod}!`,
-            icon: "success",
+          text: `Compra Realizada com Sucesso. Aqui está o Número do seu Pedido: ${response.data.id}`,
+          icon: "success",
         });
-    };
+        window.location.href = "/Home";
+      })
+  };
 
-    return (
-        <div className="flex justify-center items-center h-screen bg-gray-100 overflow-y-auto">
-            <div className="bg-white p-8 rounded-lg shadow-md w-96">
-                <h2 className="text-2xl font-semibold text-center text-gray-800 mb-6">Registro de Pagamento</h2>
 
-                <div className="space-y-4">
-                    <div>
-                        <label className="block text-gray-600">Método de Pagamento</label>
-                        <div className="flex flex-col gap-2 mt-2">
-                            <button
-                                onClick={() => setPaymentMethod("Cartão de Crédito")}
-                                className={`p-3 rounded-lg border text-gray-700 ${paymentMethod === "Cartão de Crédito" ? "bg-blue-500 text-white" : "bg-gray-200"}`}
-                            >
-                                Cartão de Crédito
-                            </button>
-                            <button
-                                onClick={() => setPaymentMethod("Cartão de Débito")}
-                                className={`p-3 rounded-lg border text-gray-700 ${paymentMethod === "Cartão de Débito" ? "bg-blue-500 text-white" : "bg-gray-200"}`}
-                            >
-                                Cartão de Débito
-                            </button>
-                            <button
-                                onClick={() => setPaymentMethod("Pix")}
-                                className={`p-3 rounded-lg border text-gray-700 ${paymentMethod === "Pix" ? "bg-blue-500 text-white" : "bg-gray-200"}`}
-                            >
-                                Pix
-                            </button>
-                        </div>
-                    </div>
-
-                    {paymentMethod === "Cartão de Crédito" || paymentMethod === "Cartão de Débito" ? (
-                        <div>
-                            <label className="block text-gray-600">Número do Cartão</label>
-                            <input
-                                type="text"
-                                placeholder="Digite o número do cartão"
-                                className="w-full p-3 mt-2 border rounded-lg bg-white text-black"
-                            />
-                        </div>
-                    ) : null}
-
-                    {paymentMethod === "Cartão de Crédito" || paymentMethod === "Cartão de Débito" ? (
-                        <div>
-                            <label className="block text-gray-600">Data de Validade</label>
-                            <input
-                                type="text"
-                                placeholder="MM/AA"
-                                className="w-full p-3 mt-2 border rounded-lg bg-white text-black"
-                            />
-                        </div>
-                    ) : null}
-
-                    {paymentMethod === "Cartão de Crédito" || paymentMethod === "Cartão de Débito" ? (
-                        <div>
-                            <label className="block text-gray-600">Código de Segurança</label>
-                            <input
-                                type="text"
-                                placeholder="Código CVV"
-                                className="w-full p-3 mt-2 border rounded-lg bg-white text-black"
-                            />
-                        </div>
-                    ) : null}
-
-                    {paymentMethod === "Pix" ? (
-                        <div>
-                            <button
-                                className="w-full bg-green-500 text-white p-3 rounded-lg hover:bg-green-600 transition duration-200"
-                            >
-                                Gerar QRCode
-                            </button>
-                        </div>
-                    ) : null}
-
-                    <div className="mt-6">
-                        <button
-                            onClick={handlePayment}
-                            className="w-full bg-green-500 text-white p-3 rounded-lg hover:bg-green-600 transition duration-200"
-                        >
-                            Finalizar Pagamento
-                        </button>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div>
+      <MainBar />
+      <div className='flex justify-center items-center flex-col bg-white min-h-screen'>
+        <div className=" flex justify-center items-center flex-col w-full bg-white rounded-lg shadow-md mb-6">
+          <h2 className="text-xl font-bold mb-4  text-black">Resumo do Pedido</h2>
+          <ul className="space-y-2">
+            {pedido.map((pedido) => (
+              <li key={pedido.pedidos.id_pedido} className="flex justify-between gap-4">
+                <span className="text-black">{pedido.produtos.nome}</span>
+                <span className="text-black">R${pedido.produtos.preco.toFixed(2)}</span>
+              </li>
+            ))}
+          </ul>
+          <div className="mt-4">
+            <p className="font-semibold text-black">Total: R${totalAmount}</p>
+          </div>
         </div>
-    );
-}
+
+        <div className="container">
+          <h1 className="title">Escolha o tipo de pagamento</h1>
+          <div className="payment-type-selection">
+            <button onClick={() => handlePaymentTypeSelect("credit")} className="button">
+              Cartão de Crédito
+            </button>
+            <button onClick={() => handlePaymentTypeSelect("debit")} className="button">
+              Cartão de Débito
+            </button>
+          </div>
+
+          {paymentType && (
+            <div className="card">
+              <h2 className="payment-title">
+                Preencha os detalhes do seu {paymentType === "credit" ? "Cartão de Crédito" : "Cartão de Débito"}
+              </h2>
+              <form onSubmit={PostPagamento} className="form">
+                <div className="input-container">
+                  <CardElement key={cardNumberKey} options={CARD_OPTIONS} />
+                  <input type="text" placeholder="CPF" className="cpf" ref={inputCPF} />
+                  <input type="text" placeholder="Nome" className="nome" ref={inputName} />
+                </div>
+
+                {/* <div className="input-row"> */}
+
+                {/* <div className="input-container">
+                  <label>CVC</label>
+                  <CardCvcElement options={CARD_OPTIONS} />
+                </div> */}
+                {/* </div> */}
+
+                <button type="submit" className="button" disabled={!stripe}>
+                  Realizar Pagamento
+                </button>
+              </form>
+              {error && <p className="error">{error}</p>}
+              {paymentMethod && <p className="success">PaymentMethod: {paymentMethod}</p>}
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const PaymentPage = () => (
+  <Elements stripe={stripePromise}>
+    <CheckoutForm />
+  </Elements>
+);
+
+export default PaymentPage;
