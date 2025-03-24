43a1d8cc0f8badb4641810071e406006e785a1b3
jPedroAs <joaopedroa2120@outlook.com>
2025-03-24 11:23:19 -0300

    md url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL:"https://eiif5bjnih.execute-api.us-east-1.amazonaws.com",
+    // baseURL:"https://eiif5bjnih.execute-api.us-east-1.amazonaws.com",
+    baseURL:"https://santapiapi.onrender.com",
     headers: {
         'Content-Type': 'application/json',
     }
